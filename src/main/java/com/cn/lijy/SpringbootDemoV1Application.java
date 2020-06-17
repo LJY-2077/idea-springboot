50e58e228cbe19330934bde87ca3f4d287046243
lijy <lijingyang@yondervision.com.cn>
2020-06-17 10:49:25 +0800

    spirngboot

---
+++ src/main/java/com/cn/lijy/SpringbootDemoV1Application.java
@@ -0,0 +1,13 @@
+package com.cn.lijy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cn/lijy/SpringbootDemoV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.lijy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
