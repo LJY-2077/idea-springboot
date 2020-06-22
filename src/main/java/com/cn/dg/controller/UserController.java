461bf2c252eedacbc78410019831a52b531f3675
lijy <lijingyang@yondervision.com.cn>
2020-06-22 18:04:38 +0800

    ztree url跳转 ifram

---
+++ src/main/java/com/cn/dg/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.cn.dg.controller;
+
+import com.cn.dg.bean.system.DgSysUser;
+import com.cn.dg.mapper.system.DgSysUserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * @author lijy
+ * @descriptin
+ * @since
+ */
+@Controller
+@Slf4j
+public class UserController {
+    @Autowired
+    DgSysUserMapper userMapper;
+
+    @RequestMapping("/user/add")
+    public String userAdd() {
+        return "/pageuser/user_add";
+    }
+
+    @RequestMapping("/user/list")
+    public String userlist(Model model) {
+
+        List<DgSysUser> users = userMapper.selectByExample(null);
+        model.addAttribute("users",users);
+        return "/pageuser/user_list";
+    }
+
+    @RequestMapping("/user/del")
+    public String userDel() {
+        return "/pageuser/user_del";
+    }
+}
+
